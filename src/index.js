4d2e334fe24c55b5d22f587fc3f55c7738367306
Raihan Zakariya <raihanzakariya49@gmail.com>
2024-02-18 16:32:49 +0530

    First commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Cards from "./Card";
+ReactDOM.render(
+  <>
+    <Cards
+      imgsrc="C:\Users\hp\Desktop\React\pract01\facebook.png"
+      title="A Real Website there"
+      sname="FaceBook"
+      link="https://www.facebook.com/campaign/landing.php?campaign_id=14884913640&extra_1=s%7Cc%7C550525804797%7Cb%7Cfacebook%7C&placement=&creative=550525804797&keyword=facebook&partner_id=googlesem&extra_2=campaignid%3D14884913640%26adgroupid%3D128696220912%26matchtype%3Db%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-592856129%26loc_physical_ms%3D9302227%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=Cj0KCQiA2eKtBhDcARIsAEGTG40zUOo146hhZqyR5o4L9lFyjD3xi4vlZeGf_twK1RjN_Lbd8HTaptYaArmWEALw_wcB"
+    />
+    <Cards
+      imgsrc="./insta.jng"
+      title="A Real Website there"
+      sname="FaceBook"
+      link="https://www.facebook.com/campaign/landing.php?campaign_id=14884913640&extra_1=s%7Cc%7C550525804797%7Cb%7Cfacebook%7C&placement=&creative=550525804797&keyword=facebook&partner_id=googlesem&extra_2=campaignid%3D14884913640%26adgroupid%3D128696220912%26matchtype%3Db%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-592856129%26loc_physical_ms%3D9302227%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=Cj0KCQiA2eKtBhDcARIsAEGTG40zUOo146hhZqyR5o4L9lFyjD3xi4vlZeGf_twK1RjN_Lbd8HTaptYaArmWEALw_wcB"
+    />
+    <Cards
+      imgsrc="./whatsapp.jng"
+      title="A Real Website there"
+      sname="FaceBook"
+      link="https://www.facebook.com/campaign/landing.php?campaign_id=14884913640&extra_1=s%7Cc%7C550525804797%7Cb%7Cfacebook%7C&placement=&creative=550525804797&keyword=facebook&partner_id=googlesem&extra_2=campaignid%3D14884913640%26adgroupid%3D128696220912%26matchtype%3Db%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-592856129%26loc_physical_ms%3D9302227%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=Cj0KCQiA2eKtBhDcARIsAEGTG40zUOo146hhZqyR5o4L9lFyjD3xi4vlZeGf_twK1RjN_Lbd8HTaptYaArmWEALw_wcB"
+    />
+    <Cards
+      imgsrc="./telegram.png"
+      title="A Real Website there"
+      sname="FaceBook"
+      link="https://www.facebook.com/campaign/landing.php?campaign_id=14884913640&extra_1=s%7Cc%7C550525804797%7Cb%7Cfacebook%7C&placement=&creative=550525804797&keyword=facebook&partner_id=googlesem&extra_2=campaignid%3D14884913640%26adgroupid%3D128696220912%26matchtype%3Db%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-592856129%26loc_physical_ms%3D9302227%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=Cj0KCQiA2eKtBhDcARIsAEGTG40zUOo146hhZqyR5o4L9lFyjD3xi4vlZeGf_twK1RjN_Lbd8HTaptYaArmWEALw_wcB"
+    />
+  </>,
+
+  document.getElementById("root")
+);
